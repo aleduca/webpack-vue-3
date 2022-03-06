f59509d99a58fb1df2cad81ac3fa5a31133a1b1c
Alexandre Cardoso <xandecar@hotmail.com>
2022-03-05 23:06:22 -0300

    Primiero commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:'./src/main.js'
+}
